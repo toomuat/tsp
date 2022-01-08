90734a6bbc3681f661423188d0107c3fcfd45e57
tom_in_wsl <32962031+toomuat@users.noreply.github.com>
2022-01-08 21:11:14 +0900

    gnuplot

--- src/main.rs
+++ src/main.rs
@@ -14,6 +14,8 @@ fn main() {
     let max_y: i32 = cities.iter().map(|t| t.1 as i32).max().unwrap();
 
     let mut gp = Command::new("gnuplot")
+        .arg("-geometry")
+        .arg("640x480")
         .stdin(Stdio::piped())
         .stderr(Stdio::piped())
         .stdout(Stdio::piped())
@@ -36,45 +38,27 @@ fn main() {
         .write_all(cmd.as_bytes())
         .unwrap();
 
-    // let commands: Vec<&str> = vec![
-    //     "set key outside\n",
-    //     "set key font'Arial,15'\n",
-    //     // "set parametric\n",
-    //     // "set style data linespoints\n",
-    //     // "set term gif animate delay 1\n",
-    //     // "set output 'tsp.gif'\n\n",
-    //     // "plot '-' pt 7 ps 1 lc rgb 'black', plot '-' pt 7 ps 1 lc rgb 'red'\n",
-    //     "plot '-' pt 7 ps 1 lc rgb 'black'\n",
-    // ];
+    let commands: Vec<&str> = vec![
+        "unset key\n",
+        "set term gif animate delay 1\n",
+        "set output 'tsp.gif'\n\n",
+    ];
 
-    // for cmd in commands.iter() {
-    //     gp.stdin
-    //         .as_mut()
-    //         .unwrap()
-    //         .write_all(cmd.as_bytes())
-    //         .unwrap();
-    // }
-
-    // Plot all cities
-    // for i in 0..cities.len() {
-    //     let cmd = format!("{} {}\n", cities[i].0, cities[i].1);
-    //     let cmd: &str = &cmd;
-
-    //     gp.stdin
-    //         .as_mut()
-    //         .unwrap()
-    //         .write_all(cmd.as_bytes())
-    //         .unwrap();
-    // }
-    // // End data input
-    // gp.stdin.as_mut().unwrap().write_all(b"e\n").unwrap();
+    for cmd in commands.iter() {
+        gp.stdin
+            .as_mut()
+            .unwrap()
+            .write_all(cmd.as_bytes())
+            .unwrap();
+    }
 
     // Plot optimal path
     let start_city = cities[0];
     let mut current_city = start_city;
     let mut visit_cities: Vec<bool> = vec![false; cities.len()];
     let mut optimal_path: Vec<(f32, f32)> = Vec::new();
-    let mut next_city: (f32, f32);
+    let mut next_city: (f32, f32) = cities[1];
+    let mut city_idx = 1;
 
     visit_cities[0] = true;
     optimal_path.push(start_city);
@@ -82,16 +66,14 @@ fn main() {
     println!("{} {}", start_city.0, start_city.1);
 
     for i in 0..cities.len() - 1 {
-        let mut next_city = cities[i];
         let mut min_dist = f32::MAX;
-        let mut city_idx = 0;
 
         for j in 1..cities.len() {
             let dist = distance(current_city, cities[j]);
             if dist < min_dist && !visit_cities[j] {
                 next_city = cities[j];
                 city_idx = j;
-                // println!("{} {}", next_city.0, next_city.1);
+                min_dist = dist;
             }
         }
 
@@ -100,16 +82,7 @@ fn main() {
         visit_cities[city_idx] = true;
 
         let commands: Vec<&str> = vec![
-            "set key outside\n",
-            "set key font'Arial,15'\n",
-            "unset key\n",
-            // "set style line 1 lt 7\n",
-            // "set style data linespoints\n",
-            // "plot '-' pt 7 ps 1 lc rgb 'black'\n",
-            // "plot '-' lp lc rgb 'cyan' lw 2 pt 5\n",
-            // "plot '-' pt 7 ps 1 lc rgb 'black', '-' lp lc rgb 'cyan' lw 4 pt 5 ps 3\n",
-            // "plot '-' pt 7 ps 1 lc rgb 'black', '-' ls 1\n",
-            "plot '-' with point pointtype 7 linecolor rgb 'black',",
+            "plot '-' with point pointtype 7 pointsize 2 linecolor rgb 'black',",
             "'-' with line linewidth 5 linetype 1 linecolor rgb 'cyan'\n",
         ];
         for cmd in commands.iter() {
@@ -121,8 +94,8 @@ fn main() {
         }
 
         // Plot all cities
-        for i in 0..cities.len() {
-            let cmd = format!("{} {}\n", cities[i].0, cities[i].1);
+        for j in 0..cities.len() {
+            let cmd = format!("{} {}\n", cities[j].0, cities[j].1);
             let cmd: &str = &cmd;
 
             gp.stdin
@@ -134,9 +107,8 @@ fn main() {
         // End data input
         gp.stdin.as_mut().unwrap().write_all(b"e\n").unwrap();
 
-        for i in 0..optimal_path.len() {
-            let cmd = format!("{} {}\n", optimal_path[i].0, optimal_path[i].1);
-            // dbg!(cmd.clone());
+        for j in 0..optimal_path.len() {
+            let cmd = format!("{} {}\n", optimal_path[j].0, optimal_path[j].1);
             let cmd: &str = &cmd;
 
             gp.stdin
@@ -145,10 +117,21 @@ fn main() {
                 .write_all(cmd.as_bytes())
                 .unwrap();
         }
+
+        if i == cities.len() - 2 {
+            let cmd = format!("{} {}\n", start_city.0, start_city.1);
+            let cmd: &str = &cmd;
+            gp.stdin
+                .as_mut()
+                .unwrap()
+                .write_all(cmd.as_bytes())
+                .unwrap();
+        }
+
         // End data input
         gp.stdin.as_mut().unwrap().write_all(b"e\n").unwrap();
 
-        std::thread::sleep(std::time::Duration::from_millis(3000));
+        // std::thread::sleep(std::time::Duration::from_millis(200));
     }
 
     replot(&mut gp);
